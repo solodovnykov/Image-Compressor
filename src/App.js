962f9cc7b196755bda7bb1c7d10276f878ed1caf
solodovnykov <solodovnikov.toni@gmail.com>
2021-03-01 12:47:20 +0200

    Fixed bg

--- src/App.js
+++ src/App.js
@@ -1,76 +1,15 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import imageCompression from "browser-image-compression";
-import imgZone from "./img/imageZone.svg";
-import { useDropzone } from "react-dropzone";
 import Card from "./components/Card/Card";
+import Info from "./components/Info/Info";
 
 function App() {
-  const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
-
-  const files = acceptedFiles.map((file) => (
-    <li key={file.path}>
-      {file.path} - {file.size} bytes
-    </li>
-  ));
-
-  const [originalImage, setOriginalImage] = useState("");
-  const [originalImageFile, setOriginalImageFile] = useState("");
-  const [compressImage, setCompressImage] = useState("");
-  const [fileName, setFileName] = useState("");
-  const [imageSize, setImageSize] = useState(1920);
-  const [compressProgress, setCompressProgress] = useState(0);
-  const [dropzone, setDropzone] = useState("");
-
-  useEffect(() => {
-    setOriginalImage(acceptedFiles[0]);
-    if (acceptedFiles[0]) {
-      setOriginalImageFile(URL.createObjectURL(acceptedFiles[0]));
-      setFileName(acceptedFiles[0].name);
-    }
-  }, [acceptedFiles]);
-
-  useEffect(() => {
-    console.log("Lol");
-  }, [files[0]]);
-
-  const handler = (e) => {
-    const imageFile = e.target.files[0];
-    setOriginalImage(imageFile);
-    setOriginalImageFile(URL.createObjectURL(imageFile));
-    setFileName(imageFile.name);
-    // console.log(URL.createObjectURL(imageFile));
-  };
-
-  const compressImageHandler = (e) => {
-    e.preventDefault();
-
-    const oprions = {
-      maxSizeMB: 1,
-      maxWidthOrHeight: imageSize,
-      useWebWorker: true,
-      onProgress: (e) => setCompressProgress(e),
-    };
-
-    if (Option.maxSizeMB >= originalImage / 1024) {
-      alert("Image is too small");
-      return 0;
-    }
-
-    let output;
-    imageCompression(originalImage, oprions).then((x) => {
-      output = x;
-
-      const downloadLink = URL.createObjectURL(output);
-      setCompressImage(downloadLink);
-    });
-  };
-
   return (
     <div className="main">
       <div className="green1"></div>
       <div className="green2"></div>
+      <div className="bg"></div>
       <div className="wrapper">
+        <Info />
         <Card />
       </div>
     </div>
---
+++ src/components/Info/Info.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Info.css";
+
+export default function Info() {
+  return (
+    <div className="info">
+      <div className="info-title">
+        Image Compressor <div className="info-icon"></div>
+      </div>
+      <div className="info-subtitle">
+        It is a very simple and fast compressor for your photos.
+      </div>
+      <div className="info-text">
+        I am a student of the National University "Lviv Polytechnic" and this is
+        one of my mini-projects.
+        <br />
+        <br />
+        Enjoy_^
+      </div>
+    </div>
+  );
+}
