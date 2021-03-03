03a09c4192e151fcaec6c1fe660d02db35541812
solodovnykov <solodovnikov.toni@gmail.com>
2021-03-03 12:34:16 +0200

    Added https redirect

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import "./App.css";
 import Card from "./components/Card/Card";
 import Info from "./components/Info/Info";
+import HttpsRedirect from "react-https-redirect";
 
 function App() {
   return (
-    <div className="main">
-      <div className="green1"></div>
-      <div className="green2"></div>
-      <div className="bg"></div>
-      <div className="wrapper">
-        <Info />
-        <Card />
+    <HttpsRedirect>
+      <div className="main">
+        <div className="green1"></div>
+        <div className="green2"></div>
+        <div className="bg"></div>
+        <div className="wrapper">
+          <Info />
+          <Card />
+        </div>
       </div>
-    </div>
+    </HttpsRedirect>
   );
 }
 
