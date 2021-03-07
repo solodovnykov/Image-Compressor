5b840162829f6011d58f08ad5a03688ae76a0a51
Anton Solodovnykov <solodovnikov.toni@gmail.com>
2021-03-07 15:34:42 +0200

    Added Header Component

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import "./App.css";
 import HttpsRedirect from "react-https-redirect";
 import Info from "./components/Card/Card";
 import Card from "./components/Info/Info";
+import Header from "./components/Header/Header";
 
 function App() {
-
   return (
     <HttpsRedirect>
-        <div className="main">
-          <div className="green1"></div>
-          <div className="green2"></div>
-          <div className="bg"></div>
-          <div className="wrapper">
-            <Info />
-            <Card />
-          </div>
+      <div className="main">
+        <div className="green1"></div>
+        <div className="green2"></div>
+        <div className="bg"></div>
+        <div className="wrapper">
+          <Header />
+          <Info />
+          <Card />
         </div>
+      </div>
     </HttpsRedirect>
   );
 }
